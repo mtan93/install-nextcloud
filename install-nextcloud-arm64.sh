a08816f0a980edc4cd367e407e2f7488a24f2b41
rieger::CLOUD <riegercloud@users.noreply.github.com>
2018-10-08 11:49:37 +0200

    Feature-Policy
    Make use of "add_header Feature-Policy "geolocation 'self'""

--- install-nextcloud-arm64.sh
+++ install-nextcloud-arm64.sh
@@ -3,9 +3,9 @@
 # https://www.c-rieger.de
 # https://github.com/riegercloud
 # INSTALL-NEXTCLOUD-ARM64.SH
-# Version 6.1 (ARM64)
+# Version 6.2 (ARM64)
 # OpenSSL 1.1.1, TLSv1.3
-# October, 05th 2018
+# October, 08th 2018
 ################################################
 # Ubuntu 18.04 LTS AMD64 - Nextcloud 14
 ################################################
@@ -462,6 +462,7 @@ add_header X-Permitted-Cross-Domain-Policies none;
 add_header X-Content-Type-Options "nosniff" always;
 add_header X-XSS-Protection "1; mode=block" always;
 add_header Referrer-Policy "no-referrer" always;
+add_header Feature-Policy "geolocation 'self'"
 EOF
 touch /etc/nginx/optimization.conf
 cat <<EOF >/etc/nginx/optimization.conf
