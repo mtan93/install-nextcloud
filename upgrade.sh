943fdb4b75b657a8c58180e104cbd86bbeb89477
rieger::CLOUD <riegercloud@users.noreply.github.com>
2018-12-10 14:07:21 +0100

    Nextcloud update using cli only
    Find more information here: https://www.c-rieger.de/nextcloud-update-using-cli-only/

---
+++ upgrade.sh
@@ -0,0 +1,14 @@
+# Ubuntu 18.04.1
+#!/bin/bash
+/usr/sbin/service nginx stop
+sudo -u www-data php /var/www/nextcloud/updater/updater.phar
+sudo -u www-data php /var/www/nextcloud/occ status
+sudo -u www-data php /var/www/nextcloud/occ -V
+sudo -u www-data php /var/www/nextcloud/occ db:add-missing-indices
+sudo -u www-data php /var/www/nextcloud/occ db:convert-filecache-bigint
+sudo -u www-data sed -i "s/upload_max_filesize=.*/upload_max_filesize=10240M/" /var/www/nextcloud/.user.ini
+sudo -u www-data sed -i "s/post_max_size=.*/post_max_size=10240M/" /var/www/nextcloud/.user.ini
+sudo -u www-data sed -i "s/output_buffering=.*/output_buffering='Off'/" /var/www/nextcloud/.user.ini
+/usr/sbin/service php7.2-fpm restart
+/usr/sbin/service nginx restart
+exit 0
