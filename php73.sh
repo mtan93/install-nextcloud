e88f2d8f14c76aabcc07e5d33766b9d64b18c73d
rieger::CLOUD <riegercloud@users.noreply.github.com>
2019-01-08 19:33:07 +0100

    PHP 7.3 Beta 3

--- php73.sh
+++ php73.sh
@@ -78,6 +78,7 @@ deb-src [arch=amd64] http://nginx.org/packages/mainline/ubuntu/ bionic nginx
 deb [arch=amd64] http://ftp.hosteurope.de/mirror/mariadb.org/repo/10.3/ubuntu bionic main
 EOF
 wget http://nginx.org/keys/nginx_signing.key && apt-key add nginx_signing.key
+apt-key adv --keyserver keyserver.ubuntu.com --reckeys 4F4EA0AAE5267A6C
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 update_and_clean
 apt install software-properties-common zip unzip screen curl git wget ffmpeg libfile-fcntllock-perl -y
