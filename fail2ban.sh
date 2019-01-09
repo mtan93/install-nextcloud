177cc549a8623b4157e861675448c8927bcfec07
rieger::CLOUD <riegercloud@users.noreply.github.com>
2019-01-09 14:57:58 +0100

    Status of fail2ban

--- fail2ban.sh
+++ fail2ban.sh
@@ -1,5 +1,6 @@
 ###
-# Staus quo: Fail2Ban (issue: /usr/local/src/install-nextcloud/fail2ban.sh)
+# StatusQuo: Fail2Ban
+# please issue: /usr/local/src/install-nextcloud/fail2ban.sh
 ###
 #!/bin/bash
 fail2ban-client status nextcloud && fail2ban-client status nginx-http-auth && fail2ban-client status sshd
