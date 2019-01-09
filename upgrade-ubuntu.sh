b42b3ce052954b26ae57ef4cee422679ad2710b5
rieger::CLOUD <riegercloud@users.noreply.github.com>
2019-01-09 14:49:51 +0100

    Ready for PHP 7.3

--- upgrade-ubuntu.sh
+++ upgrade-ubuntu.sh
@@ -10,6 +10,6 @@ sudo -u www-data sed -i "s/upload_max_filesize=.*/upload_max_filesize=10240M/" /
 sudo -u www-data sed -i "s/post_max_size=.*/post_max_size=10240M/" /var/www/nextcloud/.user.ini
 sudo -u www-data sed -i "s/output_buffering=.*/output_buffering='Off'/" /var/www/nextcloud/.user.ini
 sudo -u www-data php /var/www/nextcloud/occ update:check
-/usr/sbin/service php7.2-fpm restart
+/usr/sbin/service php7.3-fpm restart
 /usr/sbin/service nginx restart
-exit 0  
+exit 0
