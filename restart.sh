0bdf5c8aed51bbc9e487049996a3727e849b7efc
rieger::CLOUD <riegercloud@users.noreply.github.com>
2019-01-09 14:45:44 +0100

    Ready for PHP7.3

--- restart.sh
+++ restart.sh
@@ -6,6 +6,6 @@
 /usr/sbin/service nginx stop
 /usr/sbin/service mysql restart
 /usr/sbin/service redis-server restart
-/usr/sbin/service php7.2-fpm restart
+/usr/sbin/service php7.3-fpm restart
 /usr/sbin/service nginx restart
 exit 0
