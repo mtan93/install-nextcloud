83acf3a29e2f902b566c5d574e631dfd98a9b8a0
rieger::CLOUD <riegercloud@users.noreply.github.com>
2019-01-25 08:10:28 +0100

    https://www.c-rieger.de/updates/
    January 25th, 2019:
    – removed duplicate entry from my.cnf and sorted alphabetically

--- install-nextcloud-debian.sh
+++ install-nextcloud-debian.sh
@@ -249,57 +249,27 @@ apt update && apt install mariadb-server -y
 mv /etc/mysql/my.cnf /etc/mysql/my.cnf.bak
 cat <<EOF >/etc/mysql/my.cnf
 [client]
-port = 3306
-socket = /var/run/mysqld/mysqld.sock
 default-character-set = utf8mb4
-[mysqld_safe]
-socket = /var/run/mysqld/mysqld.sock
-nice = 0
-[mysqld]
-user = mysql
-pid-file = /var/run/mysqld/mysqld.pid
-socket = /var/run/mysqld/mysqld.sock
 port = 3306
+socket = /var/run/mysqld/mysqld.sock
+[mysqld_safe]
+log_error=/var/log/mysql/mysql_error.log
+nice = 0
+socket = /var/run/mysqld/mysqld.sock
+[mysqld]
 basedir = /usr
-datadir = /var/lib/mysql
-tmpdir = /tmp
-lc_messages_dir = /usr/share/mysql
-lc_messages = en_US
-skip-external-locking
-skip-name-resolve
 bind-address = 127.0.0.1
-max_connections = 200
-connect_timeout = 5
-wait_timeout = 600
-max_allowed_packet = 16M
-thread_cache_size = 128
-sort_buffer_size = 4M
+binlog_format = ROW
 bulk_insert_buffer_size = 16M
-tmp_table_size = 64M
-max_heap_table_size = 64M
-myisam_recover_options = BACKUP
-key_buffer_size = 128M
-#open-files-limit = 2000
-table_open_cache = 400
-myisam_sort_buffer_size = 512M
+character-set-server = utf8mb4
+collation-server = utf8mb4_general_ci
 concurrent_insert = 2
-read_buffer_size = 2M
-read_rnd_buffer_size = 1M
-query_cache_limit = 2M
-query_cache_size = 64M
-query_cache_type = 1
-query_cache_min_res_unit = 2k
-log_warnings = 2
-slow_query_log_file = /var/log/mysql/mariadb-slow.log
-long_query_time = 1
-log_slow_verbosity = query_plan
-slow-query-log = 1
-slow-query-log-file = /var/log/mysql/slow.log
-log_bin = /var/log/mysql/mariadb-bin
-log_bin_index = /var/log/mysql/mariadb-bin.index
-expire_logs_days = 10
-max_binlog_size = 100M
+connect_timeout = 5
+datadir = /var/lib/mysql
 default_storage_engine = InnoDB
+expire_logs_days = 10
+general_log_file = /var/log/mysql/mysql.log
+general_log = 0
 innodb_buffer_pool_size = 1024M
 innodb_buffer_pool_instances = 1
 innodb_flush_log_at_trx_commit = 2
@@ -309,20 +279,50 @@ innodb_file_per_table = 1
 innodb_open_files = 400
 innodb_io_capacity = 4000
 innodb_flush_method = O_DIRECT
-character-set-server = utf8mb4
-collation-server = utf8mb4_general_ci
+key_buffer_size = 128M
+lc_messages_dir = /usr/share/mysql
+lc_messages = en_US
+log_bin = /var/log/mysql/mariadb-bin
+log_bin_index = /var/log/mysql/mariadb-bin.index
+log_error=/var/log/mysql/mysql_error.log
+log_slow_verbosity = query_plan
+log_warnings = 2
+long_query_time = 1
+max_allowed_packet = 16M
+max_binlog_size = 100M
+max_connections = 200
+max_heap_table_size = 64M
+myisam_recover_options = BACKUP
+myisam_sort_buffer_size = 512M
+port = 3306
+pid-file = /var/run/mysqld/mysqld.pid
+query_cache_limit = 2M
+query_cache_size = 64M
+query_cache_type = 1
+query_cache_min_res_unit = 2k
+read_buffer_size = 2M
+read_rnd_buffer_size = 1M
+skip-external-locking
+skip-name-resolve
+slow_query_log_file = /var/log/mysql/mariadb-slow.log
+slow-query-log = 1
+socket = /var/run/mysqld/mysqld.sock
+sort_buffer_size = 4M
+table_open_cache = 400
+thread_cache_size = 128
+tmp_table_size = 64M
+tmpdir = /tmp
 transaction_isolation = READ-COMMITTED
-binlog_format = ROW
+user = mysql
+wait_timeout = 600
 [mysqldump]
+max_allowed_packet = 16M
 quick
 quote-names
-max_allowed_packet = 16M
-[mysql]
-#no-auto-rehash    # faster start of mysql but no tab completion
 [isamchk]
-key_buffer = 16M
 !include /etc/mysql/mariadb.cnf
 !includedir /etc/mysql/conf.d/
+key_buffer = 16M
 EOF
 /usr/sbin/service mysql restart
 clear
